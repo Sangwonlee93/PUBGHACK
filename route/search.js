c34d368d7f20c3bb080ef552e253a8b3875cc0da
이상원 <lsw0833@hanmail.net>
2017-12-06 18:07:10 +0900

    version 2

--- route/search.js
+++ route/search.js
@@ -20,10 +20,24 @@ var detail = function(req,res){
   var mode = req.query.mode || req.body.mode;
   check.detaildata(id,region,mode,function(result){
       console.log(req.connection.remoteAddress + ' 가' + id + '으로 상세정보' );
-      res.render('detail.ejs',{id:id,result:result});
+      res.render('detail.ejs',{id:id,region:region,mode:mode,result:result});
+  });
+
+};
+var compare = function(req,res){
+  var id = req.query.id || req.body.id;
+  var id2 = req.query.id2 || req.body.id2;
+  var region = req.query.region || req.body.region;
+  var mode = req.query.mode || req.body.mode;
+  check.detaildata(id,region,mode,function(result){
+      check.detaildata(id2,region,mode,function(result1){
+        console.log(req.connection.remoteAddress + ' 가' + id +' 와 ' + id2 +'으로 비교');
+        res.render('compare.ejs',{id:id,id2:id2,region:region,mode:mode,result:result,result1:result1});
+      });
   });
 
 };
 module.exports.search =search;
 module.exports.home =home;
 module.exports.detail = detail;
+module.exports.compare = compare;
